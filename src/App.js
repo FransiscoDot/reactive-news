f7749be897700863eda2d3f43ca1d7322b81bab8
francesco.megna97 <francesco.megna97@gmail.com>
2018-02-14 00:59:20 +0100

    Installed antd and babel-plugin-import for automatic import css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return(
-      <div>
+      <div style={{background: "#262728"}}>
         App
       </div>
     );
